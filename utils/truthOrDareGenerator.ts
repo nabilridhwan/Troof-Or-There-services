99235d0b443b8e58ec590a84a1f00dc80fe64f1c
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-05 17:23:11 +0800

    feat: :sparkles: more random data - 0.2.2

--- utils/truthOrDareGenerator.ts
+++ utils/truthOrDareGenerator.ts
@@ -4,11 +4,24 @@
 import all_dares from "../truth_or_dare_generator/output/all_dares.json";
 import all_truths from "../truth_or_dare_generator/output/all_truths.json";
 
+import { nanoid } from "nanoid";
+import rand from "random-seed";
+
 export function get_truth() {
 	console.log("Getting truth...");
-	return all_truths[Math.floor(Math.random() * all_truths.length)];
+	const s = nanoid();
+	const r = rand.create(s);
+	const randomNum = r.intBetween(0, all_truths.length);
+
+	console.log(`[TRUTH] Random number: ${randomNum} with seed ${s}`);
+	return all_truths[randomNum];
 }
 export function get_dare() {
 	console.log("Getting dare...");
-	return all_dares[Math.floor(Math.random() * all_dares.length)];
+	const s = nanoid();
+	const r = rand.create(s);
+	const randomNum = r.intBetween(0, all_dares.length);
+
+	console.log(`[DARE] Random number: ${randomNum} with seed ${s}`);
+	return all_dares[randomNum];
 }
